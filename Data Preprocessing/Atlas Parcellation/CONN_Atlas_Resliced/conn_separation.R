12886ff651f5c27d73ccc78dec25b24ac82fa8e7
kaitlyn-fales77 <kaitlynfales@gmail.com>
2025-03-18 11:36:39 -0400

    Update conn_separation.R

--- Data Preprocessing/Atlas Parcellation/CONN_Atlas_Resliced/conn_separation.R
+++ Data Preprocessing/Atlas Parcellation/CONN_Atlas_Resliced/conn_separation.R
@@ -1,18 +1,29 @@
-setwd("C:/Users/kaitl/OneDrive - The Pennsylvania State University/Scanner Heterogeneity Project/Atlas Parcellation/CONN_atlas")
+############ Separate CONN networks atlas into four separate DMN ROI files for reslicing ###############
 
+#### Change Working Directory if Necessary ####
+#setwd("~/FC-Network-Replicability-Effects")
+
+# Set atlas parcellation directory path
+base_directory <- paste0(getwd(),"/Data Preprocessing/Atlas Parcellation")
+
+# Packages
 library(RNifti)
 
+# Obtain CONN toolbox networks atlas, download it and load it here
+# We do not include the networks.nii file as it is publicly available with a CONN toolbox account
 conn <- readNifti("networks.nii")
 
+# Separate the CONN networks atlas into four separate files - one for each ROI of the DMN
+# Write those files into the corresponding folder to be resliced in SPM12
 mpfc <- conn[,,,1]
-writeNifti(mpfc, "mpfc.nii", template = conn)
+writeNifti(mpfc, "CONN_Atlas_Resliced/mpfc.nii", template = conn)
 
 lp.l <- conn[,,,2]
-writeNifti(lp.l, "lp_l.nii", template = conn)
+writeNifti(lp.l, "CONN_Atlas_Resliced/lp_l.nii", template = conn)
 
 lp.r <- conn[,,,3]
-writeNifti(lp.r, "lp_r.nii", template = conn)
+writeNifti(lp.r, "CONN_Atlas_Resliced/lp_r.nii", template = conn)
 
 pcc <- conn[,,,4]
-writeNifti(pcc, "pcc.nii", template = conn)
+writeNifti(pcc, "CONN_Atlas_Resliced/pcc.nii", template = conn)
 
