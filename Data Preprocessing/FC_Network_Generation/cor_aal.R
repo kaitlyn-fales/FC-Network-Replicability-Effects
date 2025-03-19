f62c7089e74c6fa1180ca049f747067750b9eefc
Kaitlyn Rose Fales <krf5429@p-bc-5106.2e.hpc.psu.edu>
2025-03-19 12:18:42 -0400

    merge

--- Data Preprocessing/FC_Network_Generation/cor_aal.R
+++ Data Preprocessing/FC_Network_Generation/cor_aal.R
@@ -1,13 +1,17 @@
 remove(list = ls())
-# change paths to your local paths
-setwd("~/Library/CloudStorage/OneDrive-ThePennsylvaniaStateUniversity/Scanner Heterogeneity Project/Correlation_Matrices") # current folder
-base_path = "/Users/hyebin/Library/CloudStorage/OneDrive-ThePennsylvaniaStateUniversity/Scanner Heterogeneity Project/Preprocessed Data" # do not put / at the end
+
+# Set wd to be general repo folder
+#setwd("~/FC-Network-Replicability-Effects")
   
 #### AAL columns & weights ####
-load("../Metadata/aal.Rdata")
+load("Data Preprocessing/Metadata/aal.Rdata")
 meta_lst = meta_aal
 subfolders = names(meta_aal)
 rm(meta_aal)
+
+# Change paths as needed
+base_path = paste0(getwd(),"/Raw Data Download/Raw_Data") # path to raw data 
+setwd(paste0(getwd(),"/Data/Correlation_Matrices")) # current folder to store data
 
 # data based on "DMN ROIs (new)" sheet in "ROI_atlas_labels.xlsx" file.
 DMN.ROI_AAL <- rep(c("MPFC", "MPFC", "LP_L", "LP_R", "LP_R", "PCC", "PCC"))
